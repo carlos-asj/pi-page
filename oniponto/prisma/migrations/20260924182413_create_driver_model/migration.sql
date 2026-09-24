7285691e290fdb0c1ee7d00b3874cc38cc1231aa
CARLOS ALBERTO SILVA JUNIOR <164648114+carlos-asj@users.noreply.github.com>
2026-09-24 18:26:49 +0000

    created `compose.yaml` to configure the docker container for database and add new models for tests

---
+++ oniponto/prisma/migrations/20260924182413_create_driver_model/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Driver" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "phone" INTEGER NOT NULL,
+
+    CONSTRAINT "Driver_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Driver_email_key" ON "Driver"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Driver_phone_key" ON "Driver"("phone");
